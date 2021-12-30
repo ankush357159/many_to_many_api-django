30101ddad453e2a5a50c3bcaf00d6dc20d090c22
Ankush <ankush357159@gmail.com>
2021-12-31 01:44:58 +0530

    api

--- authors/admin.py
+++ authors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from authors.models import Book, Author
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'publisher', 'release_date']
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'biography', 'date_of_birth']
--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import routers, serializers, viewsets, status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from authors.serializer import UserSerializer, AuthorSerializer, BookSerializer
+from authors.models import Author, Book
+from rest_framework import filters
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['release_date']
+
+
